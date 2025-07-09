18776967262288bb1f8a1b7ced15534d5ec3f8aa
Landon-Pyko <pykomar2@gmail.com>
2025-07-09 11:16:59 -0500

    Starting Project

---
+++ MyWebsite/Models/ProjectCard.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class ProjectCard
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
