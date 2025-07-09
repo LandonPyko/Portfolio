884da7c9d6b2df5a90f29c1331308867b714a722
Landon-Pyko <pykomar2@gmail.com>
2025-07-09 15:33:39 -0500

    Starting to implement Bootstrap

--- MyWebsite/obj/Debug/net9.0/MyWebsite.AssemblyInfo.cs
+++ MyWebsite/obj/Debug/net9.0/MyWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52101f11b89a78ce9acf347eff5f84dead1968f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a81083f4f83a4b5ca81796559eae37ff33c97f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
